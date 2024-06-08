d39cd9cce2bb16d87f26a9931f628b7ad206a290
M.Ali Shakeel <alishakeel567567@gmail.com>
2024-06-08 11:39:23 +0500

    sample commit

--- src/App.tsx
+++ src/App.tsx
@@ -1,24 +1,52 @@
 import React from 'react';
 import logo from './logo.svg';
 import './App.css';
+import axios from 'axios';
+import '../node_modules/bootstrap/dist/css/bootstrap.css'
 
 function App() {
+  const apiget = ()=>{
+  axios.get("https://jsonplaceholder.typicode.com/users").then((response)=>{
+    console.log(response)
+  })
+  .catch((error)=>{
+    console.log(error)
+  })}
+  const apiput =()=>{
+    axios.put('https://jsonplaceholder.typicode.com/users/1',{
+      name:"ali",
+      email:"oerpdj@gmail.com"
+    }).then((res)=>{
+      console.log(res)
+    }).catch((err)=>{
+      console.log(err)
+    })
+  }
+  const apipost =()=>{
+    axios.post('https://jsonplaceholder.typicode.com/users',{
+name : "Sahin",
+username: "iftikar",
+email:"imrankhan804@gmail.com"
+    }).then((res)=>{
+      console.log(res)
+    }).catch((err)=>{
+      console.log(err)
+    })
+  }
+  const apidel =()=>{
+    axios.delete('https://jsonplaceholder.typicode.com/users/1').then((res)=>{
+      console.log(res)
+    }).catch((err)=>{
+      console.log(err)
+    })
+  }
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.tsx</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+     <h1 className='text-center mt-5 flex-wrap text-primary'>API Handeling</h1>
+     <button className='btn btn-primary rounded-4 m-2 flex-wrap' onClick={apiget}>Get Api</button><br />
+     <button className='btn btn-primary rounded-4 m-2 flex-wrap' onClick={apiput}>Put Api</button><br />
+     <button className='btn btn-primary rounded-4 m-2 flex-wrap' onClick={apipost}>Post Api</button><br />
+     <button className='btn btn-primary rounded-4 m-2 flex-wrap'onClick={apidel}>Delete Api</button><br />
     </div>
   );
 }
